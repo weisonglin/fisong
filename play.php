8ffd4be6dfaff7f9d32b99ddf40a2cb33cbd5b8a
Victor <victorlin1234567@yahoo.com>
2016-02-20 06:24:54 -0800

    play.php

--- play.php
+++ play.php
@@ -31,29 +31,3 @@ if($_SESSION['name']==null)
       <li><a href="logout.php">Logout</a></li>
       </ul>
   </ul>
-
-
-        <body>
-            <div class="t">
-                <h1  id="fisongT"> fiSong Best Matching</h1>
-            </div>
-
-            <div class="container">
-              <h2 >fiSong Best Matching</h2>
-              <form role="form">
-                <div class="form-group">
-                  <label for="title">Matching:</label>
-                  <input type="title" class="form-control" id="email" placeholder="Enter any words here">
-                </div>
-                <!-- <div class="form-group">
-                  <label for="pwd">Password:</label>
-                  <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-                </div>
-                <div class="checkbox">
-                  <label><input type="checkbox"> Remember me</label>
-                </div> -->
-                <button type="submit" class="btn btn-primary btn-lg btn-block">fiSong</button>
-              </form>
-            </div>
-        </body>
-   
