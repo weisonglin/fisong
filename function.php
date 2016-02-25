3873283299bfa9e2502e940057ee50456e297801
Victor <victorlin1234567@yahoo.com>
2016-02-25 03:07:16 -0800

    function for kmp and lcs

--- function.php
+++ function.php
@@ -2,17 +2,85 @@
 
 
 
-function static getconnection()
+function getconnection()
 {
 	$connection=mysql_connect("db4free.net","weisong","victor1234") or die("host connection error");
 	mysql_select_db("fisonguser",$connection) or die("database error");
 }
 
+function contoarray($argv)
+{
+    $str=strtolower($argv);
+    $result=explode(" ", $str);
+    return $result;
+}
 
+function kmptable($P)
+{
+    $length = count($P);
+    $shifttable = array();
+    $shifttable[0] = 1;        
+    for ($index = 1; $index < $length; $index++) {
+        if ($P[$index]==$P[$index-$shifttable[$index-1]]) {
+            $shifttable[$index]=$shifttable[$index-1];
+        }
+        else
+            $shifttable[$index]=$index+1;
+    }
+    return $shifttable;
+}
+function kmp($T, $W, $P)
+{
+    $Plen=count($P);
+    $Wlen=count($W);
+    $result=0;
+    for($index=0; $index<$Plen;$index++)
+    {
+        $Windex=0;
+        for(; $index<$Plen&&$Windex<$Wlen; $Windex++)
+        {
+            if($P[$index]==$W[$Windex])
+            {
+                $index++;
+            }else
+            {
+                $index+=$T[$Windex]-1;;
+                break;
+            }
+        }
+        //$result=max($result,$Windex);
+        if($Windex==$Wlen)
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
 
-
-
-
+function LCS($P, $W)
+{
+    $Plen=count($P);
+    $Wlen=count($W);
+    $a=array();
+    $matrix=array();
+    $a= array_pad($a,$Wlen+1,0);
+    $matrix=array_pad($matrix,$Plen+1,$a);
+    for($Pindex=0;$Pindex<$Plen;$Pindex++)
+    {
+        for($Windex=0;$Windex<$Wlen;$Windex++)
+        {
+            if($P[$Pindex]==$W[$Windex])
+            {
+                $matrix[$Pindex+1][$Windex+1]=$matrix[$Pindex][$Windex]+1;
+            }
+            else
+            {
+                $matrix[$Pindex+1][$Windex+1]=max($matrix[$Pindex][$Windex+1],$matrix[$Pindex+1][$Windex]);
+            }
+        }
+    }
+    return $matrix[$Plen][$Wlen];
+}
 
 
 
--- test.php
+++ test.php
@@ -1,60 +1,31 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <title>fiSong</title>
-            <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <link rel='icon' href='http://localhost/example/image/m0.jpg'>
+<?php
+require_once('function.php');
+
+
+$test="I Love YOu do you love me we are happy family";
+
+$test2=contoarray($test);
+//print_r($test2);
+//echo PHP_EOL;
+$word="fuck I love me fuck family";
+$word2=contoarray($word);
+//print_r($word2);
+//echo PHP_EOL;
+
+// $table=kmptable($word2);
+// print_r($table);
+// echo PHP_EOL;
+// if(kmp($table, $word2, $test2))
+// {
+// 	echo "fuck";
+// }else
+// {
+// 	echo "no f";
+// }
+$result=LCS($test2,$word2);
+
+echo $result;
 
 
 
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-            <link rel="stylesheet" href="http://localhost/example/css/style.css">
-        </head>
-
-        <body>
-
-            <div class="content">
-                <div class="row">
-                    <h1 class="text-center"> Welcome to fiSong </h1>
-                    <h3 class="text-center"> Discover, Soul, Song</h3>
-                </div>
-
-                <div class="row">
-                  <div class="col-md-4">
-                    <h4 class='text-center'>Title</h4>
-                    <a href="http://www.sfsuswe.com/~s15g05/search.php?by=16" class="">
-                        
-                        <div class='quicksearchthumbnail thumbnail'>
-                            <img src='http://localhost/example/image/m1.jpg' alt='#' class='img-rounded'  style="width:300px;height:300px">
-                            
-                        </div>
-                    </a>
-                  </div>
-
-                  <div class="col-md-4">
-                     <h4 class='text-center'>Matching</h4>
-                    <a href="http://www.sfsuswe.com/~s15g05/search.php?by=16" class="">
-                       
-                        <div class='quicksearchthumbnail thumbnail'>
-                            <img src='http://localhost/example/image/m2.jpg' alt='#' class='img-rounded'  style="width:300px;height:300px">
-                            
-                        </div>
-                    </a>
-                  </div>
-
-                  <div class="col-md-4">
-                     <h4 class='text-center'>recommandation</h4>
-                    <a href="http://www.sfsuswe.com/~s15g05/search.php?by=16" class="">
-                        
-                        <div class='quicksearchthumbnail thumbnail'>
-                            <img src='http://localhost/example/image/m3.jpg' alt='#' class='img-rounded'  style="width:300px;height:300px">
-                           
-                        </div>
-                    </a>
-                  </div>
-                </div>
-            </div>
-        </body>
-    </html>
+?>
